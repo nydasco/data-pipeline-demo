afd5682b0cdbd512ece04414a12e862c15bf1556
Andy Sawyer <96326877+nydasco@users.noreply.github.com>
2024-02-11 07:04:25 +1100

    Update params.py
    Spacing

--- elt/params.py
+++ elt/params.py
@@ -1,6 +1,6 @@
 storage_options = {
     "AWS_ACCESS_KEY_ID": "CTvEpVXPKyOr2wwBWoHL",
-    "AWS_SECRET_ACCESS_KEY":"mTFa0ly4KXREkI1Kd2ngUsbNOpO19CiMlzQxTnQ3",
+    "AWS_SECRET_ACCESS_KEY": "mTFa0ly4KXREkI1Kd2ngUsbNOpO19CiMlzQxTnQ3",
     "AWS_REGION": "us-east-1",
     "AWS_ENDPOINT_URL": "http://localhost:9000",
     "AWS_ALLOW_HTTP": "TRUE",
