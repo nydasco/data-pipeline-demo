26b5a51ad548709557b0fe5979d1ff0693bbece7
Andy Sawyer <andrew.sawyer@lendi.com.au>
2024-02-18 15:16:26 +1100

    Update dag order

--- dags/data-pipeline-demo.py
+++ dags/data-pipeline-demo.py
@@ -67,4 +67,4 @@ with DAG(
     )
  
 start_task >> [get_rates_to_bronze >> transform_rates_to_silver >> present_rates_in_gold,
-               get_currencies_to_bronze >> transform_currencies_to_silver, present_currencies_in_gold] >> present_dates_in_gold >> end_task
+               get_currencies_to_bronze >> transform_currencies_to_silver >> present_currencies_in_gold] >> present_dates_in_gold >> end_task
