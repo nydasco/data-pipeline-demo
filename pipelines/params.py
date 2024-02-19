2b63a246a822b8de4f0c1aac9076586ded2cca27
Andy Sawyer <96326877+nydasco@users.noreply.github.com>
2024-02-19 15:00:08 +1100

    Update params.py
    Note added about API key

--- pipelines/params.py
+++ pipelines/params.py
@@ -1,6 +1,6 @@
 class Params(object):
 
-    api_key = "86HSVB5OHEPVZB9J"
+    api_key = "86HSVB5OHEPVZB9J" # This key isn't registered to anyone, but it only allows 25 API calls per day. I'd recommend getting your own.
 
     rates = [
                 {"AUD": "USD"}, 
